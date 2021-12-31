1bb916aa529dfab7cb32452931dc12d8fb75e4e8
colinrjrobbins <colinrjrobbins@gmail.com>
2021-12-31 06:23:26 +0000

    updated and cleaned code.

--- bot.py
+++ bot.py
@@ -7,10 +7,12 @@
 
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 from modules.IPGather import IPGather
 from modules.ServerCheck import ServerCheck
 
-TOKEN = os.getenv('TOKEN')
+load_dotenv()
+TOKEN = os.environ.get("TOKEN")
 
 bot = commands.Bot(command_prefix='$')
 
@@ -22,16 +24,11 @@ async def server_check(ctx):
     server_check = sc.server_running()
     print(f'Server Running: {server_check}')
     server_ip = ipg.run_ip_check()
-    with open('localIP.txt', 'w') as ip_file:
-        ip_file.write(server_ip)
+
     print(f'IP Address: {server_ip}')
     print('Server Port: 16261')
 
     response = f'Server Running: {server_check}\nIP Address: {server_ip}\nServer Port: 16261'
     await ctx.send(response)
-
-# check to see if IP changed 
-    # if IP has changed update the file
-    # send new IP to discord chat. 
 
 bot.run(TOKEN)
